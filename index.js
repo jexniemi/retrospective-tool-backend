b408574e53b33179e2a48d1e297b55f023e31cff
Jeremi Nieminen <jeremi.nieminen@cgi.com>
2018-03-25 19:22:49 +0300

    eslint configured for backend

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
 
 // Middleware
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Routers
-const examplesRouter = require('./controllers/examples')
-app.use('/api/examples', examplesRouter)
+const examplesRouter = require('./controllers/examples');
+app.use('/api/examples', examplesRouter);
 
 // Database connection
 mongoose
   .connect(config.mongoUrl)
   .then(() => {
-    console.log('connected to database', config.mongoUrl)
+    console.log('connected to database', config.mongoUrl);
   })
   .catch(err => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
 // Initialize server
-const PORT = config.port
-const server = http.createServer(app)
-server.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`)
-})
+const PORT = config.port;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${config.port}`);
+});
 
 server.on('close', () => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
 
 module.exports = {
   app, server
-}
+};
