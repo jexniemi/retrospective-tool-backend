c2f77ae773771934e0c5d0303d05789ef15409de
Olli <oltsuwh@gmail.com>
2018-03-30 18:59:39 +0300

    T15 - Authorize into an individual project from backend

--- utils/config.js
+++ utils/config.js
@@ -5,9 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
 
-if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'test') {
   port = process.env.TEST_PORT
   mongoUrl = process.env.TEST_MONGODB_URI
+}
+
+if (process.env.NODE_ENV === 'development') {
+  port = process.env.DEV_PORT
+  mongoUrl = process.env.DEV_MONGODB_URI
 }
   
 module.exports = {
