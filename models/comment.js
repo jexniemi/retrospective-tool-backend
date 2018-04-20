4824c2ba1cc6dee675422364d35041948723ffe2
Olli <oltsuwh@gmail.com>
2018-04-20 12:45:13 +0300

    T23 - Add 'time' and 'important' fields to comment model

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   content: String,
   creator: String,
+  time: { type: Date, default: Date.now },
+  important: { type: Boolean, default: false },
   project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' }
 });
 
