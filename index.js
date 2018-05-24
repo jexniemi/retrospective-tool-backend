799b07bbc190de0cbf5a061cbddd4f7d3efac9ed
Olli <oltsuwh@gmail.com>
2018-05-25 01:16:10 +0300

    T35 - require username when user logs into a project and create pointing poker

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const socketIo = require('socket.io');
 const config = require('./utils/config');
+const shortid = require('shortid');
 
 // Middleware
 app.use(cors());
@@ -48,7 +49,12 @@ server.listen(PORT, () => {
 const io = socketIo(server);
 io.of('/api/projects').on('connection', socket => {
   const name = socket.handshake.query.name;
+  const username = socket.handshake.query.username;
   socket.join(name);
+
+  const id = shortid.generate();
+  socket.emit('set id', id);
+  socket.to(name).broadcast.emit('joined', { id, username });
 
   socket.on('add', (data) => {
     socket.to(name).broadcast.emit('add comment', data);
@@ -60,6 +66,39 @@ io.of('/api/projects').on('connection', socket => {
 
   socket.on('remove', (data) => {
     socket.to(name).broadcast.emit('remove comment', data);
+  });
+
+  socket.on('introduce', (data) => {
+    socket.to(name).broadcast.emit('introduce', data);
+  });
+
+  socket.on('disconnect', () => {
+    socket.to(name).broadcast.emit('disconnected', id);
+  });
+
+  socket.on('voted', (data) => {
+    socket.to(name).broadcast.emit('voted', data);
+  });
+
+  socket.on('show votes', () => {
+    socket.emit('show votes');
+    socket.to(name).emit('show votes');
+  });
+
+  socket.on('my vote', (data) => {
+    socket.to(name).broadcast.emit('my vote', data);
+  });
+
+  socket.on('clear votes', () => {
+    socket.to(name).broadcast.emit('clear votes');
+  });
+
+  socket.on('init game', (data) => {
+    socket.to(name).emit('current game state', data.currentState);
+  });
+
+  socket.on('set poker title', (title) => {
+    socket.to(name).broadcast.emit('set poker title', title);
   });
 });
 
